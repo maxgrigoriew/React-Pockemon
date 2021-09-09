af95383228287985c28cd61c13edf10ac9c10e93
Max <sss123123111@yandex.ru>
2021-09-09 21:04:13 +0300

    add homeworc-2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './style.module.css'
 
-const Header = () => {
-    return (
-        <header className="root">
-            <div className={ styles.forest}></div>
-            <div className={styles.container}>
-                <h1>This is title</h1>
-                <p>This is Description</p>
-            </div>
-        </header>
-    )
+const Header = (props) => {
+return (
+  <header className={styles.root}>
+    <div className={styles.forest}></div>
+    <div className={styles.container}>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+    </div>
+  </header>
+);
 }
+
 export default Header;
--- src/components/HeaderBlock/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './style.module.css';
-
-
-const HeaderBlock = () => {
-    return <h1 className={styles.header}>Title</h1>
-}
-
-export default HeaderBlock;
